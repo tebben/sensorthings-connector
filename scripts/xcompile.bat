87c613c216ef829d164754c0effd30b5630081c5
tebben <ebben.tim@gmail.com>
2016-06-04 13:15:41 +0200

    fixed xcompile.bat

--- scripts/xcompile.bat
+++ scripts/xcompile.bat
@@ -5,27 +5,24 @@ mkdir ..\bin\win32
 mkdir ..\bin\win64
 mkdir ..\bin\linux32
 mkdir ..\bin\linux64
-mkdir ..\bin\linuxARMv6
 mkdir ..\bin\linuxARMv7
-mkdir ..\bin\darwin32
-mkdir ..\bin\darwin64
 
 SET GOOS=windows
 SET GOARCH=386
-go build -o ..\bin\win32\sensorthings-connector.exe ../src/main.go
+go build -o ..\bin\win32\sensorthings-connector.exe ../src/...
 echo "Built application for Windows/386"
 SET GOARCH=amd64
-go build -o ..\bin\win64\sensorthings-connector.exe ../src/main.go
+go build -o ..\bin\win64\sensorthings-connector.exe ../src/...
 echo "Built application for Windows/amd64"
 
 SET GOOS=linux
 SET GOARCH=386
-go build -o ..\bin\linux32\sensorthings-connector ../src/main.go
+go build -o ..\bin\linux32\sensorthings-connector ../src/...
 echo "Built application for Linux/386"
 SET GOARCH=amd64
-go build -o ..\bin\linux64\sensorthings-connector ../src/main.go
+go build -o ..\bin\linux64\sensorthings-connector ../src/...
 echo "Built application for Linux/amd64"
 SET GOARCH=arm
 SET GOARM=7
-go build -o ..\bin\linuxARMv7\sensorthings-connector ../src/main.go
+go build -o ..\bin\linuxARMv7\sensorthings-connector ../src/...
 echo "Built application for Linux/ARMv7"
